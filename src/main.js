2fef240127bc4967f745a9c3382b8e990238ee71
anku598 <anik598@gmail.com>
2020-11-02 22:38:27 +0600

    deploy

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { BootstrapVue } from 'bootstrap-vue'
 import Multiselect from 'vue-multiselect'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
-import "vue-multiselect/dist/vue-multiselect.min.css"
+// import "vue-multiselect/dist/vue-multiselect.min.css"
 
 Vue.component('multiselect', Multiselect)
 Vue.config.productionTip = false
